7a370c5e0a844215c4006c9ba6cbbd1d667c4371
Muhammad Rafiqi Liputo <deyurapb@gmail.com>
2024-05-27 17:35:27 +0700

    Update handler.lua

--- kong/plugins/xml-json-transformer/handler.lua
+++ kong/plugins/xml-json-transformer/handler.lua
@@ -22,31 +22,48 @@ function xml_json_transformer:new()
 end
 
 function xml_json_transformer:header_filter(conf)
-    -- setting the content type to application/json
+   -- setting the content type to application/json
     ngx.header["content-type"] = "application/json"
        -- removing content-length header to allow dynamic response size
         ngx.header["content-length"] = nil
     end
 
+    --local function xmlToJson(xmlString)
+    --  local parser = xml2lua.parser(handler)
+    --  parser:parse(xmlString)
+    --  return handler.root
+    --end
     local function xmlToJson(xmlString)
+            print("---- start fuc xmlString ------")
+            print(xmlString)
+            print("------end fuc xmlString-----")
         local tree = handler:new()
         local parser = xml2lua.parser(tree)
         parser:parse(xmlString)
         -- local root = parser.root
         if tree.root.xml then
+            print("------end fuc xmlString 111-----")
           jsonres = tree.root.xml
         else
+            print("------end fuc xmlString 222-----")
           jsonres = tree.root
         end
+          -- Menghapus kembali elemen root setelah parsing
             local jsonString = cjson.encode(jsonres)
 
             return jsonString
         end
 
         function xml_json_transformer:body_filter(conf)
-            -- Body filter phase: code here will execute on the response body from the upstream service
+            -- Body filter phase: code here will execute on the response body from the upst>
             local chunk = ngx.arg[1]
             local eof = ngx.arg[2]
+            print("-start line eof-")-----
+            print(eof)
+            print("----end line eof------:")
+            print("---- start chunk ------")
+            print(chunk)
+            print("------end chunk-")-----
 
             -- Check if the chunk is not nil and not empty
             if chunk and chunk ~= "" then
@@ -54,13 +71,20 @@ function xml_json_transformer:header_filter(conf)
                 ngx.ctx.buffer = ngx.ctx.buffer or ""
                 ngx.ctx.buffer = ngx.ctx.buffer .. chunk
             end
-
     -- If it's the last chunk of the response body
     if eof then
         local xmlString = ngx.ctx.buffer
+         print("---- start xmlstring ------")
+         print(xmlString)
+         print("------end xmlstring-----")
         -- Convert XML to JSON
         if xmlString then
             local jsonResponse = xmlToJson(xmlString)
+         print("---- start jsonResponse ------")
+         print(jsonResponse)
+         print("------end jsonResponse-----")
+
+         print("---- start jsonString ---")
 
             ngx.arg[1] = jsonResponse
          -- Membersihkan buffer setelah respons selesai diproses
